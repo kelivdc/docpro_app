3f29c371a7319ea812d6f2e69853087783f50dbc
Arie <kelivdc@yahoo.com>
2026-07-25 17:00:14 +0700

    Config

--- src/start.ts
+++ src/start.ts
@@ -3,10 +3,7 @@ import { createCsrfMiddleware, createStart } from '@tanstack/react-start'
 export const startInstance = createStart(() => ({
   requestMiddleware: [
     createCsrfMiddleware({
-      filter: (ctx) => {
-        const url = new URL(ctx.request.url)
-        return !url.pathname.startsWith('/api/auth')
-      },
+      filter: () => false,
       allowRequestsWithoutOriginCheck: true,
     }),
   ],
