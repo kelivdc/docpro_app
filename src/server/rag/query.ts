24f96367de7b04d715c162d9a317b31d9f925d98
Arie <kelivdc@yahoo.com>
2026-07-29 21:54:49 +0700

    scrollbar

--- src/server/rag/query.ts
+++ src/server/rag/query.ts
@@ -118,7 +118,7 @@ export interface ChatAnswer {
 
 export class ChatLimitError extends Error {
   constructor(limit: number, current: number) {
-    super(`Batas token bulanan (${current.toLocaleString('id-ID')} / ${limit.toLocaleString('id-ID')}) tercapai. Upgrade paket untuk token lebih banyak.`)
+    super(`Monthly token limit (${current.toLocaleString('en-US')} / ${limit.toLocaleString('en-US')}) reached. Upgrade your plan for more tokens.`)
     this.name = 'ChatLimitError'
   }
 }
